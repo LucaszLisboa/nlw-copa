b34e59b4c4cd6e298b8b59049b20bb09aefbc88d
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2022-11-02 21:55:40 -0300

    configurando ambiente mobile

---
+++ web/pages/index.tsx
@@ -0,0 +1,22 @@
+//JSX - Javascript + XML (HTML)
+//TSX - TypeScript + JSX
+interface HomeProps{
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <h1>Contagem: {props.count} </h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3030/pools/count")
+  const data = await response.json()
+
+  return{
+    props: {
+      count: data.count,
+    }
+  }
+}
